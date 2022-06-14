2bf4a15ce111929b75b9386ab56311fa3419619d
Min Woo Kim <sys_call@kakao.com>
2022-06-14 11:50:03 +0900

    initial commit

---
+++ react-master-class/src/App.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+`;
+const Input = styled.input.attrs({ require: true, minLength: 10 })`
+  background-color: tomato;
+`;
+
+function App() {
+  return (
+    <Father as="header">
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+    </Father>
+  );
+}
+
+export default App;
