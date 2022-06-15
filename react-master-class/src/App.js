b1cab2ba3e827f2adcbe5418e003db846acfaf59
Mccreate <minwoo6951@naver.com>
2022-06-15 20:15:56 +0900

    animations

--- react-master-class/src/App.js
+++ react-master-class/src/App.js
@@ -1,19 +1,51 @@
 import styled from "styled-components";
+import { keyframes } from "styled-components";
 
-const Father = styled.div`
-  display: flex;
+const Wrapper = styled.div`
+	display: flex;
 `;
-const Input = styled.input.attrs({ require: true, minLength: 10 })`
-  background-color: tomato;
+
+const rotateAnimation = keyframes`
+  0%{
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+  50%{
+    border-radius: 100px;
+  }
+  100%{
+    transform: rotate(360deg) ;
+    border-radius: 0px;
+  }
+`;
+
+const Box = styled.div`
+	width: 200px;
+	height: 200px;
+	display: flex;
+	background-color: tomato;
+	animation: ${rotateAnimation} 5s linear infinite;
+	align-items: center;
+	justify-content: center;
+	span {
+		font-size: 36px;
+		&:hover {
+			font-size: 48px;
+		}
+		&:active {
+			opacity: 0;
+		}
+	}
 `;
 
 function App() {
-  return (
-    <Father as="header">
-      <Input />
-      <Input />
-    </Father>
-  );
+	return (
+		<Wrapper>
+			<Box>
+				<span>🤩</span>
+			</Box>
+		</Wrapper>
+	);
 }
 
 export default App;
